8283c6bc8c58927e5a1ef3c3f0fe28863c40b87c
Gayathri <honeyguptha5@gmail.com>
2024-12-13 14:29:04 -0600

    flask web app integration and deployment

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV #can also apply RandomizeSearchCV
 
 from src.exception import CustomException
 
@@ -54,7 +54,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
